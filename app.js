e47c7978887c73dfa31a81d3a5e718b834526f98
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-05 00:18:29 +0300

    комментарий к коммиту

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 const bodyParser = require('body-parser');
-const { routes } = require('./routes');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
@@ -20,7 +19,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(routes);
+app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
