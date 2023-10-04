2e23486bf7007f07464402ce248388fe79440a39
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-04 21:40:03 +0300

    комментарий к коммиту

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const routes = express.Router();
+const NOTFOUND_ERROR_CODE = 404;
+
+routes.use('/', (req, res) => {
+  res.status(NOTFOUND_ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден или был запрошен несуществующий роут' });
+});
 
 routes.get('/', (req, res) => {
   res.send('Hello World!');
