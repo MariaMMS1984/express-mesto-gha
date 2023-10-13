9a75cf58ce86bf46e5e876bbdde379ea880392d3
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-13 21:13:43 +0300

    комментарий к коммиту

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,20 +5,23 @@ const JWT_SECRET = 'secret';
 const UnauthorizedError = require('../errors/autharization');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
 
-  if (!token) {
-    throw new UnauthorizedError('Для выполнения действия необходима авторизация.');
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError({ message: 'Необходима авторизация' });
   }
+
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new UnauthorizedError('Для выполнения действия необходима авторизация.');
+    throw new UnauthorizedError({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
-  next();
+
+  return next();
 };
