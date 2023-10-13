2ec8589d9e95f31a3eb1cf4b51d295a1b105f462
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-13 18:48:44 +0300

    комментарий к коммиту

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2),
+  }),
+}), createUser);
 app.use(auth);
 app.use('/', require('./routes/index'));
 
