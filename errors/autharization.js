85e292f2b72bd15dcdddd5d715f8b56fb1c2f47c
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-13 14:27:09 +0300

    комментарий к коммиту

--- errors/autharization.js
+++ errors/autharization.js
@@ -1,6 +1,7 @@
 module.exports = class UnauthorizedError extends Error {
-  constructor(message) {
+  constructor(message = 'Необходима авторизация.') {
     super(message);
+    this.message = (`401 Unauthorized — ${message}`);
     this.statusCode = 401;
   }
 };
