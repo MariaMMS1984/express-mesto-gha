92af03d1430ed63b74b1437bdf27136493fb6ee8
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-15 10:33:28 +0300

    комментарий к коммиту

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const UnauthorizedError = require('../errors/autharization');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: [true, 'Необходима электронная почта.'],
+    required: [false, 'Необходима электронная почта.'],
     unique: true,
     validate: {
       validator: (email) => validator.isEmail(email),
@@ -13,7 +16,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, 'Необходим пароль.'],
+    required: [false, 'Необходим пароль.'],
     select: false,
   },
   name: {
@@ -34,5 +37,23 @@ const userSchema = new mongoose.Schema({
     validate: /https?:\/\/(www.)?[a-z0-9A-Z\-._~:/?#[\]$&'()*+@,;=]#/,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль.');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль.');
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
