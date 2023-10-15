fc9adc3941513a8722dcad3bae65119502d83577
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-15 10:29:16 +0300

    комментарий к коммиту

--- models/user.js
+++ models/user.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');
-
-const UnauthorizedError = require('../errors/autharization');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -37,23 +34,5 @@ const userSchema = new mongoose.Schema({
     validate: /https?:\/\/(www.)?[a-z0-9A-Z\-._~:/?#[\]$&'()*+@,;=]#/,
   },
 });
-
-userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email })
-    .select('+password')
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Неправильные почта или пароль.');
-      }
-
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError('Неправильные почта или пароль.');
-          }
-          return user;
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);
