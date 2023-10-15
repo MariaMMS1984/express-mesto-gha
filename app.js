23522d25cf7245c9a167b86dc3178ef720644469
Maria Matusovskaya <Mar1a13572@yandex.ru>
2023-10-15 10:38:12 +0300

    комментарий к коммиту

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', validateLogin, login);
+app.post('/signin', login);
 app.post('/signup', validateCreateUser, createUser);
 app.use(auth);
 app.use('/', require('./routes/index'));
